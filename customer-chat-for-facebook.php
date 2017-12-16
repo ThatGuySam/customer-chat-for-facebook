b3b3c0983559832b4c762de8801e2f2823072245
Sam Carlton <sam@sam.lc>
2017-12-16 09:21:20 -0600

    Version Bump

--- customer-chat-for-facebook.php
+++ customer-chat-for-facebook.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Customer Chat for Facebook
  * Plugin URI:        https://samcarlton.com/customer-chat-for-facebook
  * Description:       Adds Facebook's Customer chat plugin to your site
- * Version:           1.0.3
+ * Version:           1.0.3.1
  * Author:            Sam Carlton
  * Author URI:        http://samcarlton.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.3' );
+define( 'PLUGIN_NAME_VERSION', '1.0.3.1' );
 
 /**
  * The code that runs during plugin activation.
