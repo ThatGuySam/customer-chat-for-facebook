5cea6ffaa45dd18100fa19602a199205c428d21a
Sam Carlton <sam@sam.lc>
2017-12-16 09:03:37 -0600

    Remove unused code

--- includes/class-customer-chat-for-facebook-cmb2-functions.php
+++ includes/class-customer-chat-for-facebook-cmb2-functions.php
@@ -646,45 +646,6 @@ function ccfb_register_taxonomy_metabox() {
  */
 function ccfb_register_theme_options_metabox() {
 
-	/**
-	 * Registers options page menu item and form.
-	 */
-	$cmb_options = new_cmb2_box( array(
-		'id'           => 'ccfb_theme_options_page',
-		'title'        => esc_html__( 'Customer Chat for Facebook', 'cmb2' ),
-		'object_types' => array( 'options-page' ),
-
-		/*
-		 * The following parameters are specific to the options-page box
-		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
-		 */
-
-		'option_key'      => 'ccfb_theme_options', // The option key and admin menu page slug.
-		'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
-		// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-		'parent_slug'     => 'options-general.php', // Make options page a submenu item of the themes menu.
-		// 'capability'      => 'manage_options', // Cap required to view options-page.
-		// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-		// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-		// 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
-		// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
-		// 'message_cb'      => 'ccfb_options_page_message_callback',
-	) );
-
-	/**
-	 * Options fields ids only need
-	 * to be unique within this box.
-	 * Prefix is not needed.
-	 */
-	$cmb_options->add_field( array(
-		'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => 'bg_color',
-		'type'    => 'colorpicker',
-		'default' => '#ffffff',
-	) );
-
 }
 
 /**
