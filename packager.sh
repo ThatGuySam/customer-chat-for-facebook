3aeb0ea1778683778f3d2263c50dbfcda6cf0398
Sam Carlton <sam@sam.lc>
2019-01-12 06:15:57 -0600

    Remove env files when packaging

--- packager.sh
+++ packager.sh
@@ -16,6 +16,8 @@ rsync -arv --exclude={'vendor','node_modules','tests','.git'} customer-chat-for-
 cd customer-chat-for-facebook-packaged
 
 echo "-Cleaning in Progress..."
+rm -rf .env
+rm -rf .env.example
 rm -rf ./.git*
 rm -rf ./.sass-cache
 rm -rf ./.directory
